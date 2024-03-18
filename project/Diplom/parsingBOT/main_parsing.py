a2c5739e34b2c7de22859588326e92675633b643
ArthurRashidov <Arch3000@inbox.ru>
2024-03-18 20:21:57 +0300

    feat:change parsing bot

--- project/Diplom/parsingBOT/main_parsing.py
+++ project/Diplom/parsingBOT/main_parsing.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.service import Service
 import time
 import json
 from urllib.parse import urlparse
+from datetime import datetime
 
 # Скачиваем chromedriver, соответствующий версии вашего браузера Google Chrome
 #chromedriver = "/path/to/chromedriver"
@@ -22,10 +23,10 @@ cookiebarbtn.click()
 # находим кнопку по ее классу
 
 # Нажимаем на кнопку
-for i in range (5):
+for i in range (6):
     button_link = browser.find_element(By.CLASS_NAME, "timetable__btn")
     button_link.click()
-    time.sleep(3)
+    time.sleep(5)
 
 
 # new_url = browser.current_url
@@ -61,11 +62,11 @@ for i in range(len(all_tournaments)):
         }
     date_dict.append(date)
  
-with open("result.json", "w", encoding="utf-8") as file:
-    json.dump(date_dict, file, ensure_ascii=False, indent=4)
+# with open("C:/Users/User/Desktop/Kursovaya_last/Kutabara/tournaments_site/project/front-end/src/result.json", "w", encoding="utf-8") as file:
+#     json.dump(date_dict, file, ensure_ascii=False, indent=4)
 
-with open("result.json", "r", encoding="utf-8") as file:
-    result = json.load(file)
+# with open("result.json", "r", encoding="utf-8") as file:
+#     result = json.load(file)
 
 
 # for i in result:
@@ -83,3 +84,95 @@ with open("result.json", "r", encoding="utf-8") as file:
     
 # Закрываем браузер
 browser.quit()
+
+
+service = Service(executable_path = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe")
+options = webdriver.ChromeOptions()
+# Запускаем браузер (Google Chrome в данном случае)
+browser = webdriver.Chrome(service=service, options=options)
+
+# Открываем веб-страницу
+url = 'https://cyberxcommunity.ru/tournaments.html'
+browser.get(url)
+
+# cookiebarbtn = browser.find_element(By.CLASS_NAME, "cookie-bar__btn")
+# cookiebarbtn.click()
+# # находим кнопку по ее классу
+
+# # Нажимаем на кнопку
+# for i in range (6):
+#     button_link = browser.find_element(By.CLASS_NAME, "timetable__btn")
+#     button_link.click()
+#     time.sleep(5)
+
+
+# new_url = browser.current_url
+# browser.get(new_url)
+
+
+# Открываем новую страницу с расписанием
+
+# Открываем новую страницу
+
+# Находим все турниры
+
+# date_dict = []
+# Время
+clock = browser.find_elements(By.CLASS_NAME, "date")
+year = "2024"
+months = {
+    "января": "01",
+    "февраля": "02",
+    "марта": "03",
+    "апреля": "04",
+    "мая": "05",
+    "июня": "06",
+    "июля": "07",
+    "августа": "08",
+    "сентября": "09",
+    "октября": "10",
+    "ноября": "11",
+    "декабря": "12"
+}
+all_dates = []
+for i in range(len(clock)):
+    day, month_text = clock[i].text.split()
+    month_text = month_text.lower()
+    month = months[month_text]
+    date_with_year = f"{day}.{month}.{year}"
+    date_obj = datetime.strptime(date_with_year, "%d.%m.%Y")
+    formatted_date = date_obj.strftime("%d.%m.%Y")
+    all_dates.append(formatted_date)
+
+
+
+name = browser.find_elements(By.CLASS_NAME, "name")
+price = browser.find_elements(By.CLASS_NAME, "num")
+info = browser.find_elements(By.CLASS_NAME, "info")
+
+for info_element in info:
+    link_element = info_element.find_element(By.TAG_NAME, "a")
+    link = link_element.get_attribute("href")
+
+# Сохраняем результаты
+
+parsed_url = urlparse(url)
+site_name = parsed_url.netloc
+site_name = site_name.replace(".com", "")
+
+count = 29
+for i in range(len(name)):
+    date = {
+        "id": count,
+        "Date": all_dates[i],
+        "GameAndFormat": name[i].get_attribute('innerHTML'),
+        "PrizeFund": price[i].get_attribute('innerHTML'),
+        "StartTime": link,
+        "Link": url,
+        "Name": site_name
+        }
+    date_dict.append(date)
+    count+=1
+ 
+with open("C:/Users/User/Desktop/Kursovaya_last/Kutabara/tournaments_site/project/front-end/src/result.json", "w", encoding="utf-8") as file:
+    json.dump(date_dict, file, ensure_ascii=False, indent=4)
